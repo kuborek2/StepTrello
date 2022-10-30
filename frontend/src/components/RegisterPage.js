50a6a5a4e1a95c6dac601f11610a6543cd4d74f4
Jakub Krezolek <kuborek2@gmail.com>
2022-10-30 03:00:09 +0100

    Added basic react router set up as well as fixed footer

---
+++ frontend/src/components/RegisterPage.js
@@ -0,0 +1,13 @@
+import Footer from './Footer';
+import './RegisterPage.css'
+
+const RegisterPage = () => {
+    return (
+        <div>
+            Register here
+            <Footer/>
+        </div>
+    );
+}
+
+export default RegisterPage;
