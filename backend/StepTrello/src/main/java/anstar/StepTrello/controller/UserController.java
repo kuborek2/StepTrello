74d96864ae7f5605107a6053e0f4a434020b299d
unknown <wojtini777@gmail.com>
2022-12-13 16:14:41 +0100

    Added endpoints for board and user controller

--- backend/StepTrello/src/main/java/anstar/StepTrello/controller/UserController.java
+++ backend/StepTrello/src/main/java/anstar/StepTrello/controller/UserController.java
@@ -1,10 +1,60 @@
 package anstar.StepTrello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import anstar.StepTrello.model.BoardDto;
+import anstar.StepTrello.model.UserDto;
+import anstar.StepTrello.service.BusinessLogic;
+import anstar.StepTrello.service.impl.BusinessLogicImpl;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api")
 public class UserController {
 
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(BoardController.class);
+
+    private final BusinessLogicImpl businessLogic;
+
+    @Autowired
+    public UserController(BusinessLogicImpl businessLogic) {
+        this.businessLogic = businessLogic;
     }
+
+    @CrossOrigin
+    @PutMapping(value = "/user")
+    public ResponseEntity <Optional<UserDto>> addUser(@RequestBody UserDto userDto) {
+        LOGGER.info("Add user " + userDto.getLogin() );
+        Optional <UserDto> newUserDto = businessLogic.saveUser(userDto);
+
+        return new ResponseEntity<>(newUserDto,HttpStatus.CREATED);
+    }
+
+    @CrossOrigin
+    @DeleteMapping(value = "/user/{userName}")
+    public ResponseEntity <Optional<UserDto> > deleteUser(@RequestBody UserDto userDto){
+        LOGGER.info("Delete this user" + userDto.getLogin() );
+        businessLogic.deleteUser(userDto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @CrossOrigin
+    @GetMapping(value = "/user")
+    public ResponseEntity <UserDto>  getUser(String login) {
+        LOGGER.info("find this user" + login);
+
+        UserDto userDto =  businessLogic.getUser(login);
+
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+
+}
