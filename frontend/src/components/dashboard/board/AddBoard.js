e1d46370cc9460818d4a3bd7368faab11fe1c5e9
Jakub Krezolek <kuborek2@gmail.com>
2022-12-15 21:25:14 +0100

    Added mui automplete to add board page

--- frontend/src/components/dashboard/board/AddBoard.js
+++ frontend/src/components/dashboard/board/AddBoard.js
@@ -1,6 +1,22 @@
 import './AddBoard.css'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
-const AddBoard = () => { 
+const AddBoard = () => {
+
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 }
+  ]
+
   return (
     <div className='addFormBox'>
       <form>
@@ -8,7 +24,21 @@ const AddBoard = () => {
         <input type="text" id="login" name="login"/><br/>
 
         <label for="cemail">Collabolators:</label><br/>
-        <input type="email" id="cemail" name="cemail"/><br/>
+        <Autocomplete
+          disableClearable
+          multiple
+          id="multiple-limit-tags"
+          onChange={(event, newValue) => {
+            setSelectedOptions(newValue);
+          }}
+          isOptionEqualToValue={(option, value) => option.title === value.title}
+          options={top100Films}
+          getOptionLabel={(option) => option.title}
+          renderInput={(params) => (
+            <TextField {...params} variant="standard" sc={{ paddingLeft: '5vw' }} />
+          )}
+          sx={{ width: '20vw', marginBottom: '10px' }}
+        />
 
         <label for="password">Password:</label><br/>
         <input type="password" id="password" name="password"/><br/>
